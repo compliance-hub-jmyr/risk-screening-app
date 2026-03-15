8bc4b0b8878b7cce6fec9180ccbb0c5019321d4d
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2026-03-15 05:19:12 -0500

    feat(config): integrate HTTP client and interceptors into app config
    Add `provideHttpClient` with interceptors (`apiVersionInterceptor`, `authInterceptor`, and `errorHandlerInterceptor`) to the global application configuration for enhanced HTTP request handling.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-
 import { routes } from './app.routes';
+import { apiVersionInterceptor, authInterceptor, errorHandlerInterceptor } from '@/app/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([apiVersionInterceptor, authInterceptor, errorHandlerInterceptor]),
+    ),
     providePrimeNG({
       theme: { preset: Aura },
     }),
